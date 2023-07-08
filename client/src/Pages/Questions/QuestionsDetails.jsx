3d759724be2ca4a16e7b2fa2175422baba0b6d42
Amrit Suthar <amritsuthar19@gmail.com>
2023-07-08 21:34:55 +0530

    2nd commit

--- client/src/Pages/Questions/QuestionsDetails.jsx
+++ client/src/Pages/Questions/QuestionsDetails.jsx
@@ -22,7 +22,7 @@ const QuestionsDetails = () => {
   const dispatch = useDispatch();
   const User = useSelector((state) => state.currentUserReducer);
   const location = useLocation();
-  const url = "http://localhost:3000";
+  const url = "https://stackoverflow-clone-server-yf5w.onrender.com";
 
   const handlePostAns = (e, answerLength) => {
     e.preventDefault();
